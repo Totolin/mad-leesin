1d2845971d405230e4b7a025f3c2cf5a2a9710ef
Cristian Stefan TOTOLIN <ctotolin@bitdefender.com>
2017-03-13 17:51:59 +0200

    - Added YouTube api jar - Created a main activity to use this api - Used online example to play a video - Assigned pause functionality to button

---
+++ app/src/main/java/com/ace/ucv/mad/leesin/Config.java
@@ -0,0 +1,23 @@
+package com.ace.ucv.mad.leesin;
+
+/**
+ * Created by ctotolin on 13-Mar-17.
+ */
+
+/*
+Certificate fingerprints:
+         MD5:  C4:C7:EE:AB:EA:80:3C:BE:1B:81:13:53:20:0F:4A:DF
+         SHA1: 5F:88:37:0B:5D:D6:AF:55:3C:4D:AD:94:A7:B1:0D:FD:BA:BF:42:3E
+         SHA256: CD:71:C3:3E:AF:42:89:DC:2A:AC:4F:5E:18:73:A8:FD:47:EB:8D:BB:0D:3D:C6:9F:3C:37:B8:35:C6:A0:54:66
+         Signature algorithm name: SHA1withRSA
+         Version: 1
+
+ */
+public class Config {
+    // Google Console APIs developer key
+    // Replace this key with your's
+    public static final String DEVELOPER_KEY = "AIzaSyABYoczeHg4XABx_jMRfv-CqmA2YMsIY4A";
+
+    // YouTube video id
+    public static final String YOUTUBE_VIDEO_CODE = "_oEA18Y8gM0";
+}
--- app/src/main/java/com/ace/ucv/mad/leesin/MainActivity.java
+++ app/src/main/java/com/ace/ucv/mad/leesin/MainActivity.java
@@ -1,13 +1,86 @@
 package com.ace.ucv.mad.leesin;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity implements
+        YouTubePlayer.OnInitializedListener {
+
+    private static final int RECOVERY_DIALOG_REQUEST = 1;
+
+    // YouTube player view
+    private YouTubePlayerView youTubeView;
+    private YouTubePlayer mPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+
+        // Initializing video player with developer key
+        youTubeView.initialize(Config.DEVELOPER_KEY, this);
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                        YouTubeInitializationResult errorReason) {
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show();
+        } else {
+            String errorMessage = String.format(
+                    getString(R.string.error_player), errorReason.toString());
+            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                        YouTubePlayer player, boolean wasRestored) {
+        if (!wasRestored) {
+
+            // loadVideo() will auto play video
+            // Use cueVideo() method, if you don't want to play it automatically
+            player.loadVideo(Config.YOUTUBE_VIDEO_CODE);
+
+            // Hiding player controls
+            player.setPlayerStyle(YouTubePlayer.PlayerStyle.CHROMELESS);
+
+            // Save player for class
+            this.mPlayer = player;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_DIALOG_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(Config.DEVELOPER_KEY, this);
+        }
+    }
+
+    private YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return (YouTubePlayerView) findViewById(R.id.youtube_view);
+    }
+
+    public void buttonClick(View view) {
+        this.mPlayer.pause();
     }
 }
